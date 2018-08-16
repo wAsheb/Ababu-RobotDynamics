7367279bc75e7acff17accf2ce6067b51f2a6ac9
wAsheb <34765872+wAsheb@users.noreply.github.com>
2018-08-16 16:33:26 +0800

    Update jacobian.h

--- jacobian.h
+++ jacobian.h
@@ -1,3 +1,50 @@
+#pragma once
+
+#include "Eigen\Dense"
+#include "MathTool.h"
+//#include "Dynamics.h"
+//#include "Kinematics.h"
+
+#define epis 1e-5
+
+using namespace Eigen;
+
+
+class Jacobian
+{
+public:
+	Jacobian( int dof);
+	~Jacobian();
+
+	void calJ ( const VectorXd& q);
+	MatrixXd getJ (){return Jmat;}
+	MatrixXd getJd( const VectorXd& q, const VectorXd& qd); // Derivative of geometric jacobian w.r.t time
+	MatrixXd DLSJacobInverse(VectorXd q);  //Singular value decomposition for Jacobian Inverse
+	Vector3d *z, *w, *pd, *Oi;    //w- angualr velocity(omega)
+
+	MatrixXd Jmat,Jmat_inv;  // Jacobian Matrix.
+
+	Dynamics* dyn; //only to let Jacobion get "get_A(i)" result.
+
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+
+private:
+	int dof;
+	//MatrixXd Jmat,Jmat_inv;  // Jacobian Matrix.
+	MatrixXd Jmatd; // derivative of Jmat
+	MatrixXd Jmat_prev;
+
+	Matrix3d *R;
+	Matrix4d *T;
+	//Vector3d *z, *w, *pd, *Oi;
+
+	Matrix4d T_flange;
+	Matrix3d R_flange;
+	Matrix4d tempMat;
+	Vector3d tempVec;
+};
+
+
 Jacobian::Jacobian(int dof)
 {
 	this->dof = dof;
