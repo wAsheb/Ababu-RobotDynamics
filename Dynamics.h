0ca9533d8fd985668bb81fea60fe5536aef307fb
Shibe <34765872+wAsheb@users.noreply.github.com>
2025-02-20 22:26:59 -0800

    Update Dynamics.h

--- Dynamics.h
+++ Dynamics.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Eigen/Dense>
+#include <vector>
 #include "DHTable.h"
 
 constexpr double DEG2RAD = 0.01745329251994;
@@ -11,8 +12,14 @@ using namespace Eigen;
 
 class Dynamics {
 public:
-    Dynamics(int dof);
+    explicit Dynamics(int dof);
     ~Dynamics();
+
+    // Rule of Five: Copy/move constructors and assignment operators
+    Dynamics(const Dynamics& other);
+    Dynamics& operator=(const Dynamics& other);
+    Dynamics(Dynamics&& other) noexcept;
+    Dynamics& operator=(Dynamics&& other) noexcept;
 
     // Inverse Dynamics: get joint torque based on Newton-Euler recursive method.
     void inv(double* out_Torq, const double* q, const double* qd, const double* qdd, 
@@ -22,6 +29,7 @@ public:
                  const Vector3d& F_ex, const Vector3d& N_ext);
     VectorXd g(const VectorXd& q);
     VectorXd auxiliaryTorque(const VectorXd& qd);
+    VectorXd frictionTorque(const VectorXd& qd);
 
     Matrix4d get_A(double theta, int Axis_No);
     Matrix4d get_A(int Axis_No);
@@ -39,20 +47,25 @@ private:
     Vector3d Zero_3;
     Vector3d z0; // Z unit vector [0,0,1]^T
 
-    Matrix3d* Roi; // Rotation Matrix from root to flange i.
-    Matrix3d* Rt;  // Rotation Matrix from i to i-1.
-    Matrix4d* T;   // Transformation Matrix from i-1 to i.
-    Vector3d* bi;
-    Vector3d* P;   // Position Vector from i-1 to i.
-    Vector3d* Omg; // Angular velocity Vector i.
-    Vector3d* Omg_d; // Angular acceleration Vector i.
-    Vector3d* Ae, *Ac; // Acceleration Vector i at end/center of link i.
-    Vector3d* Fs, *Fc; // Force Vector i at start/center of link i.
-    Vector3d* Ns, *Nc; // Torque Vector i at start/center of link i.
+    std::vector<Matrix3d> Roi; // Rotation Matrix from root to flange i.
+    std::vector<Matrix3d> Rt;  // Rotation Matrix from i to i-1.
+    std::vector<Matrix4d> T;   // Transformation Matrix from i-1 to i.
+    std::vector<Vector3d> bi;
+    std::vector<Vector3d> P;   // Position Vector from i-1 to i.
+    std::vector<Vector3d> Omg; // Angular velocity Vector i.
+    std::vector<Vector3d> Omg_d; // Angular acceleration Vector i.
+    std::vector<Vector3d> Ae, Ac; // Acceleration Vector i at end/center of link i.
+    std::vector<Vector3d> Fs, Fc; // Force Vector i at start/center of link i.
+    std::vector<Vector3d> Ns, Nc; // Torque Vector i at start/center of link i.
 
     // Temporary variables for user-input conversion.
     VectorXd q_Vec, qd_Vec, qdd_Vec, torq_Vec;
     Vector3d F_ex_Vec, N_ex_Vec;
 
-    Matrix4d* matA; // Stores transformation matrix A
+    std::vector<Matrix4d> matA; // Stores transformation matrix A
+
+    // Additional parameters for gravity compensation and friction
+    Vector3d gravity = {0, 0, -9.81};
+    VectorXd coulomb_friction;
+    VectorXd viscous_friction;
 };
